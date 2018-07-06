79acd6fdab1d42a5012c9cb0265c78bd4789baa9
iuridev <iuribarreto.ib@gmail.com>
2018-07-06 02:15:06 -0300

    Inplementação de WFP

---
+++ Mini.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Mini.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Mini.Wpf
+{
+    /// <summary>
+    /// Interação lógica para MainWindow.xam
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExec_Click(object sender, RoutedEventArgs e)
+        {
+            MinimizadorC.Minimatizar(new string[] { "/o=" + txtOrigem.Text, "/d=" + txtDestino.Text });
+        }
+    }
+}
