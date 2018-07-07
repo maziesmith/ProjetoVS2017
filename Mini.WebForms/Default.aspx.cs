c96c5026fc961304f71a9c4292f1415b9470534f
iuridev <iuribarreto.ib@gmail.com>
2018-07-06 23:43:14 -0300

    Estudo de Web Forms

---
+++ Mini.WebForms/Default.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Mini.Classes;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Mini.WebForms
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnExec_Click(object sender, EventArgs e)
+        {
+            MinimizadorC.Minimatizar(new string[] {
+                "/o=" + Path.Combine(Server.MapPath("~"), txtOrigem.Text),
+                "/d=" + Path.Combine(Server.MapPath("~"), txtDestino.Text)
+            });
+        }
+    }
+}
